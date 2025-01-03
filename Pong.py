d151378dbad269718f75c724b7dec5975e95d706
vaishnaviraut12 <160615066+vaishnaviraut12@users.noreply.github.com>
2025-01-03 23:43:38 +0530

    Add files via upload

---
+++ Pong.py
@@ -0,0 +1,123 @@
+import turtle
+
+# Set up the screen
+win = turtle.Screen()
+win.title("Pong Game")
+win.bgcolor("black")
+win.setup(width=800, height=600)
+win.tracer(0)  # Stops the window from updating automatically
+
+# Score variables
+score_a = 0
+score_b = 0
+
+# Paddle A
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("white")
+paddle_a.shapesize(stretch_wid=6, stretch_len=1)
+paddle_a.penup()
+paddle_a.goto(-350, 0)
+
+# Paddle B
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape("square")
+paddle_b.color("white")
+paddle_b.shapesize(stretch_wid=6, stretch_len=1)
+paddle_b.penup()
+paddle_b.goto(350, 0)
+
+# Ball
+ball = turtle.Turtle()
+ball.speed(40)
+ball.shape("circle")
+ball.color("white")
+ball.penup()
+ball.goto(0, 0)
+ball.dx = 2
+ball.dy = 2
+
+# Pen for score display
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("Player A: 0  Player B: 0", align="center", font=("Courier", 24, "normal"))
+
+
+# Functions
+def paddle_a_up():
+    y = paddle_a.ycor()
+    if y < 250:
+        paddle_a.sety(y + 20)
+
+
+def paddle_a_down():
+    y = paddle_a.ycor()
+    if y > -240:
+        paddle_a.sety(y - 20)
+
+
+def paddle_b_up():
+    y = paddle_b.ycor()
+    if y < 250:
+        paddle_b.sety(y + 20)
+
+
+def paddle_b_down():
+    y = paddle_b.ycor()
+    if y > -240:
+        paddle_b.sety(y - 20)
+
+
+# Keyboard bindings
+win.listen()
+win.onkeypress(paddle_a_up, "w")
+win.onkeypress(paddle_a_down, "s")
+win.onkeypress(paddle_b_up, "Up")
+win.onkeypress(paddle_b_down, "Down")
+
+# Main game loop
+while True:
+    win.update()
+
+    # Move the ball
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
+
+    # Border collision (top and bottom)
+    if ball.ycor() > 290:
+        ball.sety(290)
+        ball.dy *= -1
+
+    if ball.ycor() < -290:
+        ball.sety(-290)
+        ball.dy *= -1
+
+    # Ball goes out of bounds (left and right)
+    if ball.xcor() > 390:
+        ball.goto(0, 0)
+        ball.dx *= -1
+        score_a += 1
+        pen.clear()
+        pen.write(f"Player A: {score_a}  Player B: {score_b}", align="center", font=("Courier", 24, "normal"))
+
+    if ball.xcor() < -390:
+        ball.goto(0, 0)
+        ball.dx *= -1
+        score_b += 1
+        pen.clear()
+        pen.write(f"Player A: {score_a}  Player B: {score_b}", align="center", font=("Courier", 24, "normal"))
+
+    # Paddle collision
+    if (350 > ball.xcor() > 340) and (paddle_b.ycor() + 50 > ball.ycor() > paddle_b.ycor() - 50):
+        ball.setx(340)
+        ball.dx *= -1
+
+    if (-350 < ball.xcor() < -340) and (paddle_a.ycor() + 50 > ball.ycor() > paddle_a.ycor() - 50):
+        ball.setx(-340)
+        ball.dx *= -1
